daa77c1aec5de80ba3743fb5eb28a34e3e6451eb
RandomWater <86333848+RandomWater@users.noreply.github.com>
2022-10-04 13:03:47 -0600

    added 3 questions

---
+++ script.js
@@ -0,0 +1,20 @@
+let answer
+
+answer = prompt("What is 2 * 2?")
+if (answer == "4"){
+    alert("Correct!")
+}else{
+    alert("False!")
+}
+answer = prompt("What is 4 + 6?")
+if (answer == "10"){
+    alert("Correct!")
+}else{
+    alert("False!")
+}
+answer = prompt("X + 8 = 12 What is the value of X?")
+if (answer == "4"){
+    alert("Correct!")
+}else{
+    alert("False!")
+}
