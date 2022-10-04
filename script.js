f851f5e173bb03baec6ddfaffc68e44aa8acd904
RandomWater <86333848+RandomWater@users.noreply.github.com>
2022-10-04 13:13:40 -0600

    more questions

--- script.js
+++ script.js
@@ -18,3 +18,16 @@ if (answer == "4"){
 }else{
     alert("False!")
 }
+answer = prompt("X - Y = 0 What are x and y?")
+if (answer == "anything"){
+    alert("Correct!")
+}else{
+    alert("False!")
+}
+answer = prompt("What is 6 / 2?")
+if (answer == "3"){
+    alert("Correct!")
+}else{
+    alert("False!")
+}
+alert("You did the quiz hooray!")
